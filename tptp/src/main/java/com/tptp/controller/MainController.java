457e9a001968940f36c2e5971086aa0a8b5ef246
ki5818 <ki5818@hanmail.net>
2023-07-11 16:52:42 +0900

    23/07/11 map.html index.html * index.html\ - 차트 1 차트2 차트 3 추가
    * map.html
    - geolaocation api 추가 -> 초기 위도 경도 현재 위치로
    - 카테고리 기능 추가 -> 체크박스 클릭시 그 부분만 보이게
    
    * 기타
    - properties에 ssl 추가

--- tptp/src/main/java/com/tptp/controller/MainController.java
+++ tptp/src/main/java/com/tptp/controller/MainController.java
@@ -32,7 +32,6 @@ public class MainController {
 	@GetMapping("/")
 	public String main(Model model) throws Exception {
 		System.out.println("MainContoller");
-		model.addAttribute("discription","대한민국 전국의 관광지를 소개해드립니다. 장소별, 지역별 차트를 확인 할 수 있습니다!");
 
 		//1
 		List<Tptp> regionCount = mainService.getRegionCount();
@@ -52,22 +51,22 @@ public class MainController {
 		//3-1
 		List<Tptp> regionCategoryCountNat = mainService.getRegionCategoryCountNat();
 		model.addAttribute("regionCategoryCountNat", regionCategoryCountNat);
-		System.out.println(regionCategoryCountNat);
+		
 		
 		//3-2. tptpRegionCategoryCountCul
 		List<Tptp> regionCategoryCountCul = mainService.getRegionCategoryCountCul();
 		model.addAttribute("regionCategoryCountCul", regionCategoryCountCul);
-		System.out.println(regionCategoryCountCul);
+		
 		
 		//3-1
 		List<Tptp> regionCategoryCountThem = mainService.getRegionCategoryCountThem();
 		model.addAttribute("regionCategoryCountThem", regionCategoryCountThem);
-		System.out.println(regionCategoryCountThem);
+		
 		
 		//3-1
 		List<Tptp> regionCategoryCountTour = mainService.getRegionCategoryCountTour();
 		model.addAttribute("regionCategoryCountTour", regionCategoryCountTour);
-		System.out.println(regionCategoryCountTour);
+		
 		
 		//4
 		List<Tptp> categoryDetailCount = mainService.getCategoryDetailCount();
