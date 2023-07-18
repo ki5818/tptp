a9c7d07fb9aaad76a8def2fad341cee72edfaf28
ki5818 <ki5818@hanmail.net>
2023-07-18 09:50:05 +0900

    NewCodeMapper 수정

--- tptp/src/main/java/com/tptp/mapper/NewCodeMapper.java
+++ tptp/src/main/java/com/tptp/mapper/NewCodeMapper.java
@@ -4,6 +4,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface NewCodeMapper {
-	// 코드 생성 모듈
+	// 코드 생성 모듈 
 	public String getCommonNewCode(String tableName, String columName);
 }
