2f1121854424db61791210c68bcb6d0448cfde92
minseonn <minseonnn@naver.com>
2023-07-19 10:00:06 +0900

    23-07-19 커밋

--- tptp/src/main/java/com/tptp/controller/ReviewController.java
+++ tptp/src/main/java/com/tptp/controller/ReviewController.java
@@ -6,18 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tptp.dto.Review;
 import com.tptp.dto.Tptp;
+import com.tptp.mapper.NewCodeMapper;
 import com.tptp.mapper.ReviewMapper;
 
 @Controller
 public class ReviewController {
 	private final ReviewMapper reviewMapper;
+	private final NewCodeMapper newCodeMapper;
 	
 	@Autowired
-	public ReviewController(ReviewMapper reviewMapper) {
+	public ReviewController(ReviewMapper reviewMapper, NewCodeMapper newCodeMapper) {
 		this.reviewMapper = reviewMapper;
+		this.newCodeMapper = newCodeMapper;
 	}
 	
 
@@ -33,12 +41,45 @@ public class ReviewController {
 	
 	/*07-17 게시글 작성창 만들기*/
 	
-	@GetMapping("/reviewWrite")
-	public String reviewWrite(Model model) throws Exception {
-		//List<Review> reviewList = reviewMapper.addReviewList(reviewId, placeId, );
+	@PostMapping("/reviewWrite")
+	public String reviewWrite(Review addReview) throws Exception {
+		System.out.println("reviewWrite()");
+		System.out.println(addReview);
+		addReview.setReviewId(newCodeMapper.getCommonNewCode());
 		
-		//model.addAttribute("reviewList",reviewList);
-		return "app/reviewWrite";
+		System.out.println(addReview);
+		reviewMapper.addReviewList(addReview);
+		String url = "redirect:/reviewPopup?placeId=" + addReview.getPlaceId();
+		return url;
 	}
+	
+	/*getPlaceID 받아오기*/
+	
+	@RequestMapping(value = "/reviewPopup", method = RequestMethod.GET )
+	public String reviewPopup(Model model,
+										@RequestParam("placeId") String placeId) throws Exception{
+		System.out.println(placeId);
+		List<Review> reviewList = reviewMapper.getPlaceIDReviewList(placeId);
+	
+		model.addAttribute("reviewList", reviewList);
+		model.addAttribute("placeId", placeId);
+		return "/app/reviewPopup";
+
+	}
+	
+	/* 리뷰 삭제 하기  */
+	@RequestMapping(value = "/reviewDelete", method = RequestMethod.GET )
+	public String reviewDelete( @RequestParam("reviewId") String reviewId,
+								@RequestParam("insertPw") String insertPw) throws Exception {
+		System.out.println("reviewDelete()");
+		System.out.println(reviewId);
+		//reviewMapper.delReviewList(reviewId);
+		//String url = "redirect:/reviewpopup?placeId=" + delReview.getPlaceId();
+		return null;
+		
+		/*삭제를 할 때 삭제가 되고 다시 팝업창이 떠야?하는데 이렇게 하는게
+		 맞는 지... placeId를 받아오지 못하는 것 같다. */
+	}
+	
 
 }
